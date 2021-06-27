d94b8713132cfc33313355a9f43e85a36c6e4a0c
RebelX <51200418+RebelX127001@users.noreply.github.com>
2021-06-27 07:13:24 +0100

    Update __iDer__.py

--- __iDer__.py
+++ __iDer__.py
@@ -3,7 +3,7 @@ __AUTHOR = "Idris Olanshile Jimoh"
 __DATE = "MAR 02 2020 - till date."
 __VERSION = "3"
 """
-while True: tkinte.
+while True:
     try:
         def ex():
             res = mb.askquestion ("Confirmation", "Do you really want to leave?")
